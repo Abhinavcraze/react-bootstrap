9df3e6b11e65e08f95652cb9d61cd70bc8ebf854
Abhinav <abhinavbaskaran0803@gmail.com>
2025-11-06 13:13:05 +0530

    Commit

--- react-app-2/src/DynamicPath.jsx
+++ react-app-2/src/DynamicPath.jsx
@@ -1,5 +1,5 @@
 import {useState , useEffect , createContext} from "react";
-import {Link , useNavigate} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 
 
 export const dataContext = createContext();
